9ce33358a7fe1093e95f4353d9e83e8ca4ded5dc
Sven752 <116163742+Sven752@users.noreply.github.com>
2022-10-19 12:47:12 +0200

    Update Hallo_Welt.java

--- Hello World/src/HalloWelt/Hallo_Welt.java
+++ Hello World/src/HalloWelt/Hallo_Welt.java
@@ -4,7 +4,10 @@ public class Hallo_Welt {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hallo Welt");
-		System.out.print("Hallo Welt2");
+		System.out.println("Hallo Welt2");
+		System.out.print("Hallo Welt3");
+	
+		
 	}
 
 }

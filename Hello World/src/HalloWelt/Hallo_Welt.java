a83d4ac16a5e2d1e9230094a734fa1d6025e3d1d
Sven Bergrath <Sven Bergrath@172.29.247.182>
2022-10-19 12:44:37 +0200

    Test

--- Hello World/src/HalloWelt/Hallo_Welt.java
+++ Hello World/src/HalloWelt/Hallo_Welt.java
@@ -4,7 +4,7 @@ public class Hallo_Welt {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hallo Welt");
-		System.out.print("Hallo Welt");
+		System.out.print("Hallo Welt2");
 	}
 
 }

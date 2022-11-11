3c945f3f0fb199b3ac87e3c82c9200817827f15a
Sven Bergrath <Sven Bergrath@172.29.247.237>
2022-11-11 18:52:37 +0100

    a

--- Hello World/src/softwaretechnik_uebung5/TA.java
+++ Hello World/src/softwaretechnik_uebung5/TA.java
@@ -1,5 +1,36 @@
 package softwaretechnik_uebung5;
 
-public class TA {
+import java.util.List;
 
+public class TA 
+{
+
+	private String name;
+	private List<Course> courses;
+	
+	public TA(Course course, String name)
+	{
+		courses.add(course);
+		this.name = name;
+	}
+
+	public String getName() 
+	{
+		return name;
+	}
+
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+
+	public List<Course> getCourses() 
+	{
+		return courses;
+	}
+
+	public void extendContract(Course course) 
+	{
+		this.courses.add(course);
+	}
 }
